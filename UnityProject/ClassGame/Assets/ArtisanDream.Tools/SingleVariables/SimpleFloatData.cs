3461b12b723d492d6d0955c3937ee1b488c9c069
Csblaster <calebsintay@gmail.com>
2025-11-05 13:46:21 -0700

    unitydamageandheatlh

--- UnityProject/ClassGame/Assets/ArtisanDream.Tools/SingleVariables/SimpleFloatData.cs
+++ UnityProject/ClassGame/Assets/ArtisanDream.Tools/SingleVariables/SimpleFloatData.cs
@@ -1,18 +1,32 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu(menuName = "Single Variables/SimpleFloatData")]
 public class SimpleFloatData : ScriptableObject
 {
     public float value;
-    
+
+    [Header("Optional Event When Zero")]
+    public UnityEvent onZero;
+
     public void UpdateValue(float amount)
     {
         value += amount;
+        CheckForZero();
     }
-    
-    //SetData ()
+
     public void SetValue(float amount)
     {
         value = amount;
+        CheckForZero();
+    }
+
+    private void CheckForZero()
+    {
+        if (value <= 0f)
+        {
+            value = 0f;
+            onZero?.Invoke(); // triggers whatever you assign in the Inspector
+        }
     }
 }
